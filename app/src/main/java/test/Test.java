611f8151e0190d3807376ba16f8eafc427ea339f
John <994601812@qq.com>
2016-11-27 16:39:24 +0800

    初始化项目

---
+++ app/src/main/java/test/Test.java
@@ -0,0 +1,8 @@
+package test;
+
+/**
+ * Created by John on 2016/11/27.
+ */
+
+public class Test {
+}
---
+++ app/src/main/java/test/Test2.java
@@ -0,0 +1,9 @@
+package test;
+
+/**
+ * Created by John on 2016/11/27.
+ */
+
+public class Test2 {
+
+}
